f7e73dfb3708b148688c5d30507524ad8883114e
LEE-HT <12gusxkr@naver.com>
2023-11-20 17:16:22 +0900

    letsencrypt key certbot renew sh

---
+++ letsencrypt.sh
@@ -0,0 +1,11 @@
+certbot renew
+cd /etc/letsencrypt/live/codelia.shop/
+expect <<EOF
+spawn sudo openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name tomcat -CAfile chain.pem -caname root
+expect "Password:"
+send "135792468a\r"
+expect "Password:"
+send "135792468a\r"
+expect eof
+EOF
+aws s3 cp keystore.p12 s3://aws-github-action-bucket/SSL_KEY/
