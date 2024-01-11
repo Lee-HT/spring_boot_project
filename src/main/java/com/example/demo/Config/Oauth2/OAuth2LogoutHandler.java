0f19e57c2330c6da112f0ea75e2076b63b5af54f
LEE-HT <12gusxkr@naver.com>
2024-01-11 15:55:50 +0900

    Logout Success 시에 front 쪽으로 redirect 하도록 수정

--- src/main/java/com/example/demo/Config/Oauth2/OAuth2LogoutHandler.java
+++ src/main/java/com/example/demo/Config/Oauth2/OAuth2LogoutHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,18 +18,24 @@ import org.springframework.stereotype.Component;
 public class OAuth2LogoutHandler extends SimpleUrlLogoutSuccessHandler {
 
     private final CookieProvider cookieProvider;
+    private final Environment env;
 
-    public OAuth2LogoutHandler(CookieProvider cookieProvider) {
+    public OAuth2LogoutHandler(CookieProvider cookieProvider, Environment env) {
         this.cookieProvider = cookieProvider;
+        this.env = env;
     }
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        Cookie accessToken = cookieProvider.expireCookie(JwtProperties.accessTokenName);
         Cookie refreshToken = cookieProvider.expireCookie(JwtProperties.refreshTokenName);
-        response.addCookie(accessToken);
         response.addCookie(refreshToken);
+
+        String redirectUrl = env.getProperty("front") + "/codelia_react/";
+        if (!response.isCommitted()) {
+            String encodedUrl = response.encodeRedirectURL(redirectUrl);
+            response.sendRedirect(encodedUrl);
+        }
 
         super.onLogoutSuccess(request, response, authentication);
     }
