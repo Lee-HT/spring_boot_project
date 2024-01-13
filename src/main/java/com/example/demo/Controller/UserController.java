a697a4f287d1aea65fb629da4aeab2501abbbc5c
Lee-HT <12gusxkr@naver.com>
2024-01-13 20:13:41 +0900

    유저 정보 일부 수정 메소드를 put -> patch 로 변경

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -3,13 +3,7 @@ package com.example.demo.Controller;
 import com.example.demo.DTO.UserDto;
 import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -27,7 +21,7 @@ public class UserController {
         return userService.findByUid(uid);
     }
 
-    @PutMapping
+    @PatchMapping
     public UserDto updateUser(@RequestBody UserDto userDto) {
         return userService.updateUser(userDto);
     }
